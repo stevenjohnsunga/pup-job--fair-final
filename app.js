d0c2123c38ea5df9dc0575e0a854f0c2ae1c78b9
jeruabordo <gellyneabordo@gmail.com>
2021-12-17 17:28:42 +0800

    node.js modules added. working node.js server-side js

--- app.js
+++ app.js
@@ -1,55 +1,22 @@
-// Select The Elements
-var toggle_btn;
-var big_wrapper;
-var hamburger_menu;
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
 
-function declare() {
-  toggle_btn = document.querySelector(".toggle-btn");
-  big_wrapper = document.querySelector(".big-wrapper");
-  hamburger_menu = document.querySelector(".hamburger-menu");
-}
+// __dirname : It will resolve to project folder.
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
 
-const main = document.querySelector("main");
+router.get('/stud_login', function(req,res){
+  res.sendFile(path.join(__dirname + '/stud_login.html'));
+});
 
-declare();
+// for css and img files
+app.use(express.static(path.join(__dirname, '/')));
 
-let dark = false;
+// add the router
+app.use('/', router);
+app.listen(process.env.port || 3000);
 
-function toggleAnimation() {
-  // Clone the wrapper
-  dark = !dark;
-  let clone = big_wrapper.cloneNode(true);
-  if (dark) {
-    clone.classList.remove("light");
-    clone.classList.add("dark");
-  } else {
-    clone.classList.remove("dark");
-    clone.classList.add("light");
-  }
-  clone.classList.add("copy");
-  main.appendChild(clone);
-
-  document.body.classList.add("stop-scrolling");
-
-  clone.addEventListener("animationend", () => {
-    document.body.classList.remove("stop-scrolling");
-    big_wrapper.remove();
-    clone.classList.remove("copy");
-    // Reset Variables
-    declare();
-    events();
-  });
-}
-
-function events() {
-  toggle_btn.addEventListener("click", toggleAnimation);
-  hamburger_menu.addEventListener("click", () => {
-    big_wrapper.classList.toggle("active");
-  });
-}
-
-events();
-
-
-
-
+console.log('Running at Port 3000');
