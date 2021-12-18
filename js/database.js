c2498085b7d1ee1668fc2b1589a2e54f2c6fd2e1
jeruabordo <gellyneabordo@gmail.com>
2021-12-18 16:25:15 +0800

    modify folder structure

---
+++ js/database.js
@@ -0,0 +1,39 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'AdminGellyne1998', // your password
+    database : 'test' // your database
+  });
+  
+  connection.connect((err) => {
+      if(err) throw err;
+      console.log('Connected to MySQL Server!');
+  });
+
+// const connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : 'root', // your password
+//   database : 'db_name' // your database
+// });
+
+// connection.connect((err) => {
+//     if(err) throw err;
+//     console.log('Connected to MySQL Server!');
+// });
+
+// const pool = mysql.createPool({
+//     connectionLimit : 100, //important
+//     host     : 'localhost',
+//     user     : 'root',
+//     password : 'root',
+//     database : 'db_name',
+//     debug    :  false
+// });
+
+// pool.getConnection((err, connection) => {
+//     if(err) throw err
+//     console.log('connected as id ' + connection.threadId)
+// });
