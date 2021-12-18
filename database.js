d183bc058faa36e56545da4646e194dc4ba0b6ec
jeruabordo <gellyneabordo@gmail.com>
2021-12-18 17:06:47 +0800

    minor changes

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : 'AdminGellyne1998', // your password
+    password : 'root', // your password
     database : 'test' // your database
   });
   
